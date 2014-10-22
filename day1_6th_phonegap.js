93ce0a25ee22bc1b3d4031ae1283a1aeb4a1951f
Alexis MINEAUD <crock@42.wf>
2014-10-22 11:57:14 +0200

    Update notes.

---
+++ day1_6th_phonegap.js
@@ -0,0 +1,12 @@
+# 6th presentation: Cordova
+
+- Smartphones++++, tablets+++, PC+
+- Cordova embeds webapps
+- cool CLI, node module too
+- ngCordova: ionic directives for Cordova
+- allows DI to call Cordova API
+- ngCordovaMocks (if (!window.cordova) to inject only on desktop)
+- cordova run android
+  cordova run ios
+- live debug with inspector
+
